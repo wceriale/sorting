142621c174ba34872072e8d72cbb97e474d5ded1
William <wceriale@cs.washington.edu>
2016-06-29 14:12:28 -0700

    corrected merge sort. added comments

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -22,25 +22,37 @@ public class MergeSort {
     }
 
     public static <E> E[] mergesort(E[] arr) {
-    	if(arr.length > 1) {
+        // If it's a single element, just return it
+    	if(arr.length == 1) 
+            return arr;
+        else {
+            // Split array in half
     		E[] arr1 = (E[]) new Object[arr.length / 2];
     		E[] arr2 = (E[]) new Object[arr.length - arr1.length];
+
+            // Fill halves with values
     		for(int i = 0; i < arr1.length; i++) 
     			arr1[i] = arr[i];
     		for(int i = 0; i < arr2.length; i++) 
     			arr2[i] = arr[i + arr1.length];
+
+            // Recursively sort both parts of the array
     		arr1 = mergesort(arr1);
     		arr2 = mergesort(arr2);
+
+            // Merge them together, return the result
     		return merge(arr1, arr2);
     	}
-        return arr;
     }
 
     public static <E> E[] merge(E[] arr1, E[] arr2) {
+        // Create new array which will be the result of the merge
         E[] arr = (E[]) (new Object[arr1.length + arr2.length]); 
-    	int arr1i = 0;
-    	int arr2i = 0;
-    	int index = 0;
+
+        // Initialize an index for each array
+    	int arr1i = 0; int arr2i = 0; int index = 0;
+
+        // Merge elements based on Comparator function
     	while(arr1i < arr1.length && arr2i < arr2.length) {
     		if(comparator.compare(arr1[arr1i], arr2[arr2i]) < 0) {
     			arr[index] = arr1[arr1i];
@@ -63,7 +75,8 @@ public class MergeSort {
     		index++;
     		arr2i++;
     	}
-        //System.out.println(Arrays.toString(arr));
+
+        // Return result
         return arr;
     }
 }
